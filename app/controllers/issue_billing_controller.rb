dc2678da48d79b43e5b0d97d3024c670d5b0f256
pvdvreede <paul@vdvreede.net>
2012-09-04 11:29:54 +1000

    Add PDF option, although not implemented yet.

--- app/controllers/issue_billing_controller.rb
+++ app/controllers/issue_billing_controller.rb
@@ -1,5 +1,6 @@
 class IssueBillingController < ApplicationController
   include IssueBillingHelper
+  include Redmine::Export::PDF
 
   unloadable
 
@@ -23,11 +24,18 @@ class IssueBillingController < ApplicationController
       issues_scope = issues_scope.where("issues.updated_on <= ?", @billing_filter.end_date) unless @billing_filter.end_date.nil?
     end
 
-    @issues = issues_scope.all
+    # setup pagination
+    @limit = per_page_option
+    @issue_count = issues_scope.count
+    @issue_pages = Paginator.new self, @issue_count, @limit, params['page']
+    @offset ||= @issue_pages.current.offset
+
+    @issues = issues_scope.offset(@offset).limit(@limit)
 
     respond_to do |format|
       format.html { render :template => 'issue_billing/issues' }
-      format.csv { send_data(billing_to_csv(@issues), :type => 'text/csv; header=present', :filename => 'export.csv') }
+      format.csv  { send_data(billing_to_csv(@issues), :type => 'text/csv; header=present', :filename => 'export.csv') }
+      format.pdf  { send_data(issues_to_pdf(@issues, @project, nil), :type => 'application/pdf', :filename => 'export.pdf') }
     end
   end
 
