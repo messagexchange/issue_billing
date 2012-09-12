17fd6852e7a4b546446a9c83405b014d897d1931
pvdvreede <paul@vdvreede.net>
2012-09-12 16:42:01 +1000

    Add meta data information and version number bump.

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ Redmine::Plugin.register :issue_billing do
   name 'Issue Billing plugin'
   author 'Paul Van de Vreede'
   description 'Create billing report from issue time entries.'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  version '0.8.0'
+  url 'https://github.com/messagexchange/issue_billing'
+  author_url 'https://github.com/messagexchange'
 
   # add billing permission
   permission :view_billing, :issue_billing => :issues
