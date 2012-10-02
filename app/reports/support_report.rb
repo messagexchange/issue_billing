b24ac4b8a4cded8006eee71a4b0fe03f89fa0a69
Paul Van de Vreede <paul@vdvreede.net>
2012-10-01 21:38:39 -0400

    Aesthetic improvements to PDF report.

--- app/reports/support_report.rb
+++ app/reports/support_report.rb
@@ -9,39 +9,42 @@ class SupportReport < Prawn::Document
   def to_pdf(issues, project, total_hours, start_date, end_date)
 
     image "#{IssueBilling::LOGO_URL}#{Setting.plugin_issue_billing['ib_logo_image']}", \
-          :height => 50, :vposition => :top, :position => :right
+          :height => 25, :vposition => :top, :position => :right
 
-    font_size(16) { text "Support Log" }
+    font_size(10) { text "<b>Support Log</b>", :inline_format => true }
 
-    font_size 10
+    font_size 8
 
-    move_down 10
+    move_down 4
 
     text "<b>Customer:</b> #{project.name}", :inline_format => true
 
-    move_down 10
+    move_down 4
 
     text "<b>Period:</b> #{start_date} to #{end_date} (inclusive)", :inline_format => true
 
-    move_down 10
+    move_down 4
 
     text "<b>Total hours:</b> #{total_hours}", :inline_format => true
 
-    move_down 20
+    move_down 7
 
     issues_array = create_issues_table(issues, total_hours)
 
-    font_size 8
-    table(issues_array, :header => true, :cell_style => { :border_width => 0, :inline_format => true })
+    font_size 6
+    table(issues_array, :header => true, :width => (bounds.right - 1), :cell_style => { :border_width => 0, :inline_format => true }) do
+      row(0).background_color = "CCCCCC"
+      cells.padding = 1
+
+    end
 
     page_string = "page <page> of <total>"
 
-    # number_pages page_string, {
-    #   :align => :right,
-    #   :width => 50,
-    #   :at => [bounds.right - 70, bounds.bottom + 8],
-    #   :overflow => :expand
-    # }
+    number_pages page_string, {
+      :align => :right,
+      :width => 50,
+      :at => [bounds.right - 70, -2]
+    }
 
     render
   end
